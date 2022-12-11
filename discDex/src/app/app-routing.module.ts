7e58e512e66573fabed2a7c5414b1ac1b1f995fb
Eric Klomp <ericklomp@Erics-MacBook-Pro.local>
2022-12-11 14:32:12 -0500

    fix routing

--- discDex/src/app/app-routing.module.ts
+++ discDex/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'discdex', component: DiscdexComponent },
   { path: 'about', component: AboutComponent },
 ];
