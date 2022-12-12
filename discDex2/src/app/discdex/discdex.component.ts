fa321f7da8299d71288e5dd3edfcadc546aeec49
jmw-75 <jmw75@students.calvin.edu>
2022-12-11 21:12:51 -0500

    adding2

---
+++ discDex2/src/app/discdex/discdex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+@Component({
+  selector: 'app-discdex',
+  templateUrl: './discdex.component.html',
+  styleUrls: ['./discdex.component.scss']
+})
+export class DiscdexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
